8781ef0ef338edd7725304e36af86a3b212a2ad2
fernoe1 <krutoytemirlan2007@mail.ru>
2025-10-27 18:24:23 +0600

    feat: part 4, 5, 6

--- scripts/list.js
+++ scripts/list.js
@@ -1,29 +1,34 @@
 const container = document.getElementById("tapestryContainer");
 const button = document.getElementById("seeMore");
+const loader = document.getElementById("loadingSpinner");
 
 const apiUrl = "https://kuizine-backend.onrender.com/food";
 
 button.addEventListener("click", async () => {
-    try {
-        const response = await fetch(apiUrl);
+  try {
+    loader.style.display = "inline-block";
+    button.style.display = "none";
 
-        if (!response.ok) throw new Error("Failed to fetch cuisines from api");
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error("Failed to fetch cuisines from API");
 
-        const cuisines = await response.json();
+    const cuisines = await response.json();
 
-        cuisines.forEach(cuisine => {
-            const div = document.createElement("div");
-            div.classList.add("cuisineContainer");
-            div.innerHTML = `
-            <h3>${cuisine.name}</h3>
-            <p>${cuisine.description}</p>
-            `;
-            container.insertBefore(div, button);
-        });
+    cuisines.forEach((cuisine) => {
+      const div = document.createElement("div");
+      div.classList.add("cuisineContainer");
+      div.innerHTML = `
+        <h3>${cuisine.name}</h3>
+        <p>${cuisine.description}</p>
+      `;
+      container.insertBefore(div, button);
+    });
 
-        button.hidden = true;
-
-    } catch(error) {
-        console.error(error);
-    }
+    loader.style.display = "none";
+    button.hidden = true;
+  } catch (error) {
+    console.error(error);
+    loader.style.display = "none";
+    button.style.display = "inline-block";
+  }
 });
