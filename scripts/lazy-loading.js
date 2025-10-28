9516b3ed74c7895b94caa18251ff8fbe7a7d2ce4
oomka123 <k.ahmetov123@gmail.com>
2025-10-28 17:03:44 +0500

    fix/lazy-l

--- scripts/lazy-loading.js
+++ scripts/lazy-loading.js
@@ -1,23 +1,34 @@
-$(document).ready(function () {
-  const images = $(".lazy-img");
+// Find all images with the data-src attribute
+const $imgTargets = $("img[data-src]");
 
-  // Creating IntersectionObserver
-  const observer = new IntersectionObserver(
-    function (entries, observer) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          const $img = $(entry.target);
-          $img.attr("src", $img.data("src"));
-          $img.addClass("fade-in");
-          observer.unobserve(entry.target);
-        }
-      });
-    },
-    { threshold: 0.2 }
-  );
+// Callback function for IntersectionObserver
+const loadImg = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
 
-  // Enabling the monitoring of all images
-  images.each(function () {
-    observer.observe(this);
+    const $img = $(entry.target);
+
+    // Replace src with data-src
+    $img.attr("src", $img.data("src"));
+
+    // When the image has loaded, remove "lazy-img" class
+    $img.on("load", function () {
+      $img.removeClass("lazy-img");
+    });
+
+    // Stop observing this element
+    observer.unobserve(entry.target);
   });
+};
+
+// Create the observer
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+// Observe each image
+$imgTargets.each(function () {
+  imgObserver.observe(this);
 });
